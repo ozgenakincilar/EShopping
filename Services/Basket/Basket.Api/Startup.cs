165a1a0aae61b63bdf32e7138d270e734e6e7d6c
Özgen AKINCILAR <ozgen.akincilar@veripark.com>
2024-01-30 00:47:32 +0300

    middlewares added

---
+++ Services/Basket/Basket.Api/Startup.cs
@@ -0,0 +1,71 @@
+﻿using Basket.Application.Handlers;
+using Basket.Core.Repositories;
+using Basket.Infrastructure.Repositories;
+using HealthChecks.UI.Client;
+using MediatR;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+namespace Basket.Api
+{
+    public class Startup
+    {
+        public IConfiguration Configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddApiVersioning();
+
+            //redis settings
+            services.AddStackExchangeRedisCache(opt =>
+            {
+                opt.Configuration = Configuration.GetValue<string>("CacheSettings:ConnectionString");
+            });
+            services.AddMediatR(typeof(CreateShoppingCardCommandHandler).GetTypeInfo().Assembly);
+            services.AddScoped<IBasketRepository, BasketRepository>();
+            services.AddAutoMapper(typeof(Startup));
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Basket.Api"
+                });
+            });
+
+            services.AddHealthChecks()
+                .AddRedis(Configuration["CacheSettings:ConnectionString"], "Redis Health", HealthStatus.Degraded);
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Basket.Api v1"));
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHealthChecks("/health",new HealthCheckOptions
+                {
+                    Predicate=_=> true,
+                    ResponseWriter=UIResponseWriter.WriteHealthCheckUIResponse
+                });
+            });
+        }
+    }
+}
