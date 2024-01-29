d65621cfaa45d51ae4d837f7fba7754f6b3a114a
Özgen AKINCILAR <ozgen.akincilar@veripark.com>
2024-01-30 00:51:47 +0300

    startup created

--- Services/Basket/Basket.Api/Program.cs
+++ Services/Basket/Basket.Api/Program.cs
@@ -1,37 +1,13 @@
-namespace Basket.Api
+namespace Basket.Api;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-            builder.Services.AddAuthorization();
-
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseAuthorization();
-
-            var summaries = new[]
-            {
-                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-            };
-
-         
-
-            app.Run();
-        }
+        CreateHostBuilder(args).Build().Run();
     }
-}
+
+    private static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
+};
