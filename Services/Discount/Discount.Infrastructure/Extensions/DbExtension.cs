99857c82d8ba15aa59cbcf5033f4ca6345f28202
Özgen AKINCILAR <ozgen.akincilar@veripark.com>
2024-01-30 04:30:14 +0300

    migrations added

---
+++ Services/Discount/Discount.Infrastructure/Extensions/DbExtension.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Npgsql;
+
+namespace Discount.Infrastructure.Extensions
+{
+    public static class DbExtension
+    {
+        public static IHost MigrateDatabase<TContext>(this IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var config = services.GetRequiredService<IConfiguration>();
+
+                var logger = services.GetRequiredService<ILogger<TContext>>();
+
+                try
+                {
+                    logger.LogInformation("Db migration started.");
+                    ApplyMigration(config);
+                    logger.LogInformation("Db migration completed.");
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+
+                return host;
+            }
+        }
+
+        private static void ApplyMigration(IConfiguration config)
+        {
+            var connection = new NpgsqlConnection(config.GetValue<string>("DatabaseSettings: ConnectionString"));
+            connection.Open();
+            using var cmd = new NpgsqlCommand
+            {
+                Connection = connection
+            };
+
+            cmd.CommandText = "drop table if exists Coupon ";
+            cmd.ExecuteNonQuery();
+            cmd.CommandText = @"create table Coupon(Id SERIAL PRIMARY KEY ,
+                                                  ProductName VARCHAR(500),
+                                                  Description TEXT,
+                                                  Amount INT)";
+            cmd.ExecuteNonQueryAsync();
+            cmd.CommandText = "insert into Coupon(ProductName,Description,Amount) values " +
+                                                "('Adidas Quick Force Indoor Badminton Shoes','Shoe Discount',500)";
+            cmd.ExecuteNonQuery();
+            cmd.CommandText = "insert into Coupon(ProductName,Description,Amount) values " +
+                                                "('Yonex Super Ace Light Badminton Shoes','Racquet Discount',3500)";
+            cmd.ExecuteNonQuery();
+
+        }
+    }
+
+}
+
---
+++ Services/Discount/Discount.Infrastructure/Repositories/DiscountRepository.cs
@@ -0,0 +1,82 @@
+﻿using Dapper;
+using Discount.Core.Entities;
+using Discount.Core.Repositories;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+
+namespace Discount.Infrastructure.Repositories
+{
+    public class DiscountRepository : IDiscountRepository
+    {
+        private readonly IConfiguration _configuration;
+
+        public DiscountRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<bool> CreateDiscount(Coupon coupon)
+        {
+            await using var connection = new NpgsqlConnection(_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+
+            var affected = await connection.ExecuteAsync("insert into coupon(ProductName,Description,Amount) values (@ProductName,@Amount,@Description)",
+                                            new { ProductName = coupon.ProductName, Amount = coupon.Amount, Description = coupon.Description });
+
+            if (affected == 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public async Task<bool> DeleteDiscount(string productName)
+        {
+
+            await using var connection = new NpgsqlConnection(_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+
+            var affected = await connection.ExecuteAsync("delete from coupon where ProductName=@ProductName",
+                                             new { ProductName = productName });
+
+            if (affected == 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public async Task<Coupon> GetDiscount(string productName)
+        {
+            await using var connection = new NpgsqlConnection(_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var coupon = await connection.QueryFirstOrDefaultAsync<Coupon>("select * from coupon where ProductName=@ProductName",
+                                                                            new { ProductName = productName });
+
+            if (coupon == null)
+            {
+                return new Coupon
+                {
+                    ProductName = "No Discount",
+                    Amount = 0,
+                    Description = "No discounts available."
+                };
+            }
+            return coupon;
+        }
+
+        public async Task<bool> UpdateDiscount(Coupon coupon)
+        {
+            await using var connection = new NpgsqlConnection(_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+
+            var affected = await connection.ExecuteAsync("update coupon set  ProductName=@ProductName,Amount=@Amount,Description=@Description where Id=@Id",
+                                             new { ProductName = coupon.ProductName, Amount = coupon.Amount, Description = coupon.Description ,Id=coupon.Id});
+
+            if (affected == 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
