7ff7b76b09bee347def845ce6070aee215589444
Özgen AKINCILAR <ozgen.akincilar@veripark.com>
2024-01-30 06:26:02 +0300

    overrided gRPC methods

---
+++ Services/Discount/Discount.Api/Services/DiscountService.cs
@@ -0,0 +1,69 @@
+﻿using Catalog.Application.Commands;
+using Discount.Application.Commands;
+using Discount.Application.Queries;
+using Discount.Grpc.Protos;
+using Grpc.Core;
+using MediatR;
+
+namespace Discount.Api.Services
+{
+    public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+    {
+        private readonly IMediator _mediator;
+        private readonly ILogger _logger;
+        public DiscountService(IMediator mediator, ILogger logger)
+        {
+            _mediator = mediator;
+            _logger = logger;
+        }
+
+        public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            var query = new GetDiscountQuery(request.ProductName);
+            var result = await _mediator.Send(query);
+            _logger.LogInformation($"discount retrieved for the product name: {request.ProductName} and amount : {result.Amount}");
+            return result;
+        }
+
+        public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+        {
+            var command = new CreateDiscountCommand
+            {
+                ProductName = request.Coupon.ProductName,
+                Amount = request.Coupon.Amount,
+                Description = request.Coupon.Description,
+            };
+
+            var result = await _mediator.Send(command);
+            _logger.LogInformation($"discount is successfully create for the product name : {result.ProductName}");
+            return result;
+        }
+
+        public override async Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+        {
+            var command = new UpdateDiscountCommand
+            {
+                Id = request.Coupon.Id,
+                ProductName = request.Coupon.ProductName,
+                Amount = request.Coupon.Amount,
+                Description = request.Coupon.Description,
+            };
+
+            var result = await _mediator.Send(command);
+            _logger.LogInformation($"discount is successfully create for the product name : {result.ProductName}");
+            return result;
+        }
+
+        public override async Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+        {
+            var command = new DeleteDiscountCommand(request.ProductName);
+            var deleted= await _mediator.Send(command);
+            var response = new DeleteDiscountResponse
+            {
+                Success = deleted
+            };
+
+            return response;
+        }
+    }
+}
---
+++ Services/Discount/Discount.Api/Startup.cs
@@ -0,0 +1,37 @@
+﻿using Discount.Api.Services;
+using Discount.Application.Handlers;
+using Discount.Core.Repositories;
+using MediatR;
+using System.Reflection;
+
+namespace Discount.Api
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMediatR(typeof(CreateDiscountCommandHandler).GetTypeInfo().Assembly);
+            services.AddScoped<IDiscountRepository, IDiscountRepository>();
+            services.AddAutoMapper(typeof(Startup));
+            services.AddGrpc();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGrpcService<DiscountService>();
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Communication with gRRPC endpoints must be made through a gRPC client");
+                });
+            });
+        }
+    }
+}
